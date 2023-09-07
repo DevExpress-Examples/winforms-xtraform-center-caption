ff0cd068ebb0d04524d4d5d720b4133a13b8d7b2
DevExpressExampleBot <fakeEmail>
2023-09-07 09:26:57 +0400

    Source auto update [skip ci]

--- VB/WindowsApplication1/XtraForm1.vb
+++ VB/WindowsApplication1/XtraForm1.vb
@@ -1,10 +1,8 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports DevExpress.Utils
 
 Namespace WindowsApplication1
-    Partial Public Class XtraForm1
+
+    Public Partial Class XtraForm1
         Inherits DevExpress.XtraEditors.XtraForm
 
         Public Sub New()
